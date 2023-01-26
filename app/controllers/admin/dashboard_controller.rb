01e4dd581fee63ba5d60d275eccc9fc06dc1db80
Jhon Useche <jhon.useche@telus.com>
2023-01-25 20:22:16 -0700

    Add counters to the Admin dashbaord.

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,9 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: ENV['HTTP_BASIC_USER'], password: ENV['HTTP_BASIC_PASSWORD']
+
   def show
+    @categories = Category.all
+    @products = Product.all
+    @orders = Order.all
   end
 end

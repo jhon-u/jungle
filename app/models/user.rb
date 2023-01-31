4e016955eb41d1fd2b16345d657e72b500c9ad64
Jhon Useche <jhon.useche@telus.com>
2023-01-31 09:38:39 -0700

    Add authenticate_with_credentials method.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,19 @@
 class User < ApplicationRecord
 
   has_secure_password
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 8 }
+  validates :password_confirmation, presence: true
+
+  def authenticate_with_credentials(email, password)
+    user = User.find_by_email(email.strip.downcase)
+    if user.nil? || !user.authenticate(password)
+      nil
+    else
+      user
+    end
+  end
 end
